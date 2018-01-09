fb86b167fb2c81b1506e48dee606ec3a45f29fbc
Jack Middlebrook <jack.middlebrook@gmail.com>
2018-01-09 08:19:52 +0100

    initial commit

---
+++ entertainment.py
@@ -0,0 +1,8 @@
+import media
+
+caddyshack = media.Movie("Caddyshack",
+                         "An exclusive golf course has to deal with a brash new member and a destructive dancing gopher",
+                         "https://upload.wikimedia.org/wikipedia/en/8/84/Caddyshack_poster.jpg",
+                         "https://www.youtube.com/watch?v=x9Nl39uWEYk")
+
+print(caddyshack.storyline)
