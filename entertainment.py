66d397cf22131a24efaef14cca727e6e062bbe1d
Jack Middlebrook <jack.middlebrook@gmail.com>
2018-01-09 08:58:57 +0100

    add shawshank redemption

--- entertainment.py
+++ entertainment.py
@@ -11,4 +11,15 @@ happy_gilmore = media.Movie("Happy Gilmore",
                             "https://upload.wikimedia.org/wikipedia/en/b/be/Happygilmoreposter.jpg",
                             "https://www.youtube.com/watch?v=y1emDAYCfVQ")
 
-caddyshack.show_trailer()
+gladiator = media.Movie("Gladiator",
+                        "When a Roman General is betrayed, and his family murdered by an emperor's corrupt son, "
+                        "he comes to Rome as a gladiator to seek revenge.",
+                        "https://en.wikipedia.org/wiki/Gladiator_(2000_film)#/media/File:Gladiator_ver1.jpg",
+                        "https://www.youtube.com/watch?v=owK1qxDselE")
+
+shawshank_redemption = media.Movie("Shawshank Redemption",
+                                   "Two imprisoned men bond over a number of years, finding solace and eventual "
+                                   "redemption through acts of common decency.",
+                                   "https://en.wikipedia.org/wiki/The_Shawshank_Redemption#/media/File"
+                                   ":ShawshankRedemptionMoviePoster.jpg", 
+                                   "https://www.youtube.com/watch?v=6hB3S9bIaco")
