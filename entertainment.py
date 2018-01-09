80c2910d9f972e0070c66db9a4fac779c76bbf3b
Jack Middlebrook <jack.middlebrook@gmail.com>
2018-01-09 08:23:21 +0100

    added happy gilmore

--- entertainment.py
+++ entertainment.py
@@ -5,4 +5,9 @@ caddyshack = media.Movie("Caddyshack",
                          "https://upload.wikimedia.org/wikipedia/en/8/84/Caddyshack_poster.jpg",
                          "https://www.youtube.com/watch?v=x9Nl39uWEYk")
 
-print(caddyshack.storyline)
+happy_gilmore = media.Movie("Happy Gilmore",
+                            "A rejected hockey player puts his skills to the golf course to save his grandmother's "
+                            "house",
+                            "https://upload.wikimedia.org/wikipedia/en/b/be/Happygilmoreposter.jpg",
+                            "https://www.youtube.com/watch?v=y1emDAYCfVQ")
+
