518b35eac6b587498506197194817894663ae00c
Jack Middlebrook <jack.middlebrook@gmail.com>
2018-01-10 08:29:44 +0100

    update documentation and formatting

--- media.py
+++ media.py
@@ -2,9 +2,26 @@ import webbrowser
 
 
 class Movie:
-    """This class provides a way to store movie related information"""
+    """This class provides a way to store movie related information
 
-    def __init__(self, movie_title, movie_storyline, poster_image, trailer_youtube):
+    Args:
+        movie_title (str): Movie's title
+        movie_storyline (str): Short explanation of storyline of movie
+        poster_image (str): Link to the poster image of the movie
+        trailer_youtube (str): Link to the trailer of the movie on YouTube
+
+    Attributes:
+        title (str): Movie's title
+        storyline (str): Short explanation of storyline of movie
+        poster_image_url (str): Link to the poster image of the movie
+        trailer_youtube_url (str): Link to the trailer of the movie on YouTube
+
+    Methods:
+        show_trailer : Plays the trailer from YouTube in the browser
+    """
+
+    def __init__(self, movie_title, movie_storyline, poster_image,
+                 trailer_youtube):
         self.title = movie_title
         self.storyline = movie_storyline
         self.poster_image_url = poster_image
